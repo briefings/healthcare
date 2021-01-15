62202969d9ff1f5def9bd2125b509e049bd95755
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-15 06:49:26 +0000

    Add script.sh

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# A script file for Google Colaboratory
+
+
+# logs
+rm -rf *.log && rm -rf *.pdf && rm -rf *.zip
+
+
+# dask
+pip install dask[complete] &> dask.log
+
+
+# https://linux.die.net/man/1/wget
+wget -q https://github.com/briefings/phe/raw/develop/phe.zip
+
+
+# https://linux.die.net/man/1/unzip
+# -u: update
+# -q: quiet
+rm -rf phe
+unzip -u -q phe.zip
+rm -rf phe.zip
