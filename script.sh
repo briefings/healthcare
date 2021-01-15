e79b542d0464bf0870c18f66120983484122cfd8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-15 20:14:52 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,18 +2,14 @@
 
 # A script file for Google Colaboratory
 
-
 # logs
 rm -rf *.log && rm -rf *.pdf && rm -rf *.zip
-
 
 # dask
 pip install dask[complete] &> dask.log
 
-
 # https://linux.die.net/man/1/wget
 wget -q https://github.com/briefings/phe/raw/develop/phe.zip
-
 
 # https://linux.die.net/man/1/unzip
 # -u: update
