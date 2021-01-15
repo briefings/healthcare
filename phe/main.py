0c7c9a0fc2b74c4dbb6c634b398014285a0ebfd2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-15 20:08:13 +0000

    Add phe/main.py

---
+++ phe/main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import logging
+
+
+def main():
+
+    sample, institutions, notes = phe.cases.admissionstotal.AdmissionsTotal(url=configurations.url).exc()
+    logger.info('\n{}\n\n'.format(sample.head()))
+
+    wide, narrow = phe.baselines.formatting.Formatting().exc(blob=sample)
+    logger.info('\n{}\n\n'.format(wide.head()))
+    logger.info('\n{}\n\n'.format(narrow.head()))
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    import config
+
+    configurations = config.Config()
+
+    import phe.cases.admissionstotal
+    import phe.baselines.formatting
+
+    main()
