a14b4578f62d573a722e7d59f2eee7b80c04cee1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-16 01:05:27 +0000

    Update phe/cases/admissionstotal.py

--- phe/cases/admissionstotal.py
+++ phe/cases/admissionstotal.py
@@ -3,56 +3,80 @@ import collections
 import numpy as np
 import pandas as pd
 
+import phe.algorithms.patterns
+
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 class AdmissionsTotal:
 
     def __init__(self, url):
+        """
+
+        :param url:
+        """
 
         self.url = url
         Dimensions = collections.namedtuple(typename='Dimensions', field_names=['all', 'descriptive'])
         self.dimensions = Dimensions._make((['region', 'code', 'institution'], ['region', 'institution']))
 
-        self.datacells = 'C:KM'  # type='Data', field_names=['cells', 'start', 'end']
-        self.dataend = 515
-        self.datastart = 24
+        Data = collections.namedtuple(typename='Data', field_names=['cells', 'start', 'end'])
+        self.data = Data._make(('C:KM', 24, 515))
 
-        self.fieldnames = 'F:KM'  # type='FieldNames', field_names=['cells', 'row']
-        self.fieldrow = 13
+        FieldNames = collections.namedtuple(typename='FieldNames', field_names=['cells', 'row'])
+        self.fieldnames = FieldNames._make(('F:KM', 13))
 
-        self.notes = "Provider Level Data - Admissions - Number of patients admitted with COVID-19 (Last 24hrs)"
+        self.notes = "Provider Level Data - Admissions - Number " \
+                     "of patients admitted with COVID-19 (Last 24hrs)"
 
     def readings(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
 
         try:
             return pd.read_excel(io=self.url, sheet_name='Admissions Total', header=None,
-                                 skiprows=np.arange(self.datastart - 1), usecols=self.datacells,
-                                 nrows=(self.dataend - self.datastart + 1))
+                                 skiprows=np.arange(self.data.start - 1), usecols=self.data.cells,
+                                 nrows=(self.data.end - self.data.start + 1))
         except OSError as err:
             raise Exception(err.strerror) from err
 
     def fields(self) -> list:
+        """
+
+        :return:
+        """
 
         try:
             names = pd.read_excel(
-                io=self.url, sheet_name='Admissions Total', header=None, skiprows=self.fieldrow - 1,
-                usecols=self.fieldnames, nrows=1, parse_dates=True)
+                io=self.url, sheet_name='Admissions Total', header=None, skiprows=self.fieldnames.row - 1,
+                usecols=self.fieldnames.cells, nrows=1, parse_dates=True)
         except OSError as err:
             raise Exception(err.strerror) from err
 
         return self.dimensions.all + names.astype(str).values.tolist()[0]
 
     def institutions(self, blob) -> pd.DataFrame:
+        """
+
+        :param blob:
+        :return:
+        """
 
         frame = blob[self.dimensions.all]
         assert frame.shape[0] == frame.drop_duplicates().shape[0]
+        frame = phe.algorithms.patterns.Patterns().exc(blob=frame.copy())
 
         return frame
 
     def exc(self) -> (pd.DataFrame, pd.DataFrame, str):
+        """
+
+        :return:
+        """
 
         # Bare minimum checks
-        assert self.datacells.rsplit(':')[1] == self.fieldnames.rsplit(':')[1], \
+        assert self.data.cells.rsplit(':')[1] == self.fieldnames.cells.rsplit(':')[1], \
             "The last column of the data cells must be the same as the last column " + \
             "of the fields names"
 
