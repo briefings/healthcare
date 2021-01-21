2be84ba1a3675f8bd41f042752c102d3ab2dd5ba
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-21 22:57:52 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
+
+
 class Config:
 
     def __init__(self):
         self.url = 'https://www.england.nhs.uk/statistics/wp-content/uploads/' \
                    'sites/2/2021/01/Covid-Publication-14-01-2021.xlsx'
+
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
