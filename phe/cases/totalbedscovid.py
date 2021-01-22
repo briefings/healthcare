bd9264af21d6847318964868ed807bc7be5ceb90
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-22 19:16:29 +0000

    Add phe/cases/totalbedscovid.py

---
+++ phe/cases/totalbedscovid.py
@@ -0,0 +1,91 @@
+import collections
+
+import numpy as np
+import pandas as pd
+
+import phe.algorithms.patterns
+
+
+# noinspection PyUnresolvedReferences,PyProtectedMember
+class TotalBedsCOVID:
+
+    def __init__(self, url):
+        """
+
+        :param url:
+        """
+
+        self.url = url
+        Dimensions = collections.namedtuple(typename='Dimensions', field_names=['all', 'descriptive'])
+        self.dimensions = Dimensions._make((['region', 'code', 'institution'], ['region', 'institution']))
+
+        Data = collections.namedtuple(typename='Data', field_names=['sheet_name', 'cells', 'start', 'end'])
+        self.data = Data._make(('Total Beds Occupied Covid', 'C:KM', 24, 515))
+
+        FieldNames = collections.namedtuple(typename='FieldNames', field_names=['cells', 'row'])
+        self.fieldnames = FieldNames._make(('F:KM', 13))
+
+        self.notes = "Provider Level Data - Total beds - occupied by confirmed COVID-19 patients (as at 08:00)"
+
+    def dataset(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+
+        try:
+            return pd.read_excel(io=self.url, sheet_name=self.data.sheet_name, header=None,
+                                 skiprows=np.arange(self.data.start - 1), usecols=self.data.cells,
+                                 nrows=(self.data.end - self.data.start + 1))
+        except OSError as err:
+            raise Exception(err.strerror) from err
+
+    def fields(self) -> list:
+        """
+
+        :return:
+        """
+
+        try:
+            names = pd.read_excel(
+                io=self.url, sheet_name=self.data.sheet_name, header=None, skiprows=self.fieldnames.row - 1,
+                usecols=self.fieldnames.cells, nrows=1, parse_dates=True)
+        except OSError as err:
+            raise Exception(err.strerror) from err
+
+        return self.dimensions.all + names.astype(str).values.tolist()[0]
+
+    def institutions(self, blob) -> pd.DataFrame:
+        """
+
+        :param blob:
+        :return:
+        """
+
+        frame = blob[self.dimensions.all]
+        assert frame.shape[0] == frame.drop_duplicates().shape[0]
+        frame = phe.algorithms.patterns.Patterns().exc(blob=frame.copy())
+
+        return frame
+
+    def exc(self) -> (pd.DataFrame, pd.DataFrame, str):
+        """
+
+        :return:
+        """
+
+        # Bare minimum checks
+        assert self.data.cells.rsplit(':')[1] == self.fieldnames.cells.rsplit(':')[1], \
+            "The last column of the data cells must be the same as the last column " + \
+            "of the fields names"
+
+        # Data
+        data = self.dataset()
+        data = data.set_axis(labels=self.fields(), axis=1)
+
+        # Hence
+        series = data[data.columns.drop(labels=self.dimensions.descriptive)].copy()
+        series.fillna(0, inplace=True)
+        institutions = self.institutions(blob=data)
+
+        return series, institutions, self.notes
