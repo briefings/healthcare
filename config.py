87dc1e0f356d10e8963fa67dab3a9c3365a18b55
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-01-23 00:52:07 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ import os
 class Config:
 
     def __init__(self):
-        self.url = 'https://www.england.nhs.uk/statistics/wp-content/uploads/' \
-                   'sites/2/2021/01/Covid-Publication-14-01-2021.xlsx'
+        """
+        The constructor
+        """
+
+        self.source = 'https://www.england.nhs.uk/statistics/wp-content/uploads/' \
+                      'sites/2/2021/01/Covid-Publication-14-01-2021.xlsx'
 
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+        self.uri = os.path.join(os.getcwd(), 'data', 'Covid-Publication-14-01-2021.xlsx')
